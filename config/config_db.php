7f0e41d0c58fc3759d8a00afbd84eeb7796a3b02
GeeDawn <steam66620202021@126.com>
2021-12-26 11:01:09 +0800

    person
    person

---
+++ config/config_db.php
@@ -0,0 +1,12 @@
+<?php
+/*
+db_type = "sqlite"
+db_name = "config/e0d7330e238d9d898405f79506938872.db"
+con_db_host = "localhost"
+con_db_port = "3306"
+con_db_id = "root"
+con_db_pass = "root"
+con_db_name = "metv75"
+tablepre = "met_"
+db_charset = "utf8"
+*/?>
